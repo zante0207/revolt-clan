2079be004738085ae27f2a7795d13dd0706006ea
zante0207 <zante0702@gmail.com>
2026-03-09 15:33:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+particlesJS("particles-js", {
+
+particles:{
+
+number:{value:100},
+
+size:{value:3},
+
+move:{speed:2},
+
+line_linked:{
+enable:true,
+color:"#00f7ff"
+}
+
+},
+
+interactivity:{
+
+events:{
+onhover:{enable:true,mode:"repulse"},
+onclick:{enable:true,mode:"push"}
+}
+
+}
+
+});
